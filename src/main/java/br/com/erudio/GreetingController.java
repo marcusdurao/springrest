e46b16adb626496beb428db20d56dc1947356606
MarcusDurao <mvdurao@gmail.com>
2021-11-03 22:32:20 -0300

    Primeira versao calculadora

--- src/main/java/br/com/erudio/GreetingController.java
+++ src/main/java/br/com/erudio/GreetingController.java
@@ -1,7 +1,8 @@
 package br.com.erudio;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -12,8 +13,13 @@ public class GreetingController {
     private static final String template = "hello, %s!";
     private final AtomicLong counter = new AtomicLong();
 
-    @RequestMapping("/greeting")
-    public Greeting greeting(@RequestParam(value="name", defaultValue = "Word") String name) {
-        return new Greeting(counter.incrementAndGet(), String.format(template,name));
+    @RequestMapping(value = "/sum/numberOne/numberTwo", method = RequestMethod.GET)
+    public Double sum(@PathVariable("numberOne") String numberOne, @PathVariable("numberTwo") String numberTwo) {
+        return 10.0;
     }
+//    Utilizado para enviar os parametros via queryParam
+//    @RequestMapping("/greeting")
+//    public Greeting greeting(@RequestParam(value="name", defaultValue = "Word") String name) {
+//        return new Greeting(counter.incrementAndGet(), String.format(template,name));
+//    }
 }
