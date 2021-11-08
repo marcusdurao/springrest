1aee6a1ae0bca68ff029f2437d5fb263c8cf4a51
MarcusDurao <mvdurao@gmail.com>
2021-11-08 17:21:23 -0300

    Secao 10

---
+++ src/main/resources/db/migration/V2__Insert_Person.sql
@@ -0,0 +1,3 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+	(1, 'Av. Bianco 275', 'Marcus Vinicius Durao', 'Male', 'Massucato'),
+	(2, 'Av. Bianco 275', 'Valeria', 'Famele', 'Retto');
